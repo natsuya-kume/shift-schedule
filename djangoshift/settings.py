54143dd14da11a03f9d9484e037b86f21da88a60
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2021-01-16 16:04:11 +0900

    Setting rules

--- djangoshift/settings.py
+++ djangoshift/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djangoshift.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'asia/Tokyo'
 
 USE_I18N = True
 
